7714aa25351ec31c5128d6f47a8c968134bb61fa
happygoalvin <yo.alvin.96@gmail.com>
2022-05-11 15:26:36 +0800

    Added hero to landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Hero from "../images/hero-image.jpg"
 
 export default function Landing() {
   return (
     <React.Fragment>
-      <h1>The Analog Space</h1>
+        <div
+          class="hero min-h-screen"
+          style={{backgroundImage: `url(${Hero})`}}
+        >
+          <div class="hero-overlay bg-opacity-40"></div>
+          <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+              <h1 class="mb-5 text-5xl font-bold">C A N O N - A E 1</h1>
+              <p class="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button class="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
     </React.Fragment>
   );
 }
